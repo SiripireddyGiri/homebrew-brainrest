ddf6da6dabaf49c3db4946a1be7b3d93cf673390
SiripireddyGiri <Siripireddygirikumar3@gmail.com>
2025-12-07 21:22:46 +0530

    Add Homebrew formula for brainrest

--- brainrest.rb
+++ brainrest.rb
@@ -2,9 +2,8 @@ class Brainrest < Formula
   desc "Micro-break reminder tool for terminal users"
   homepage "https://github.com/SiripireddyGiri/Brainrestall"
   url "https://raw.githubusercontent.com/SiripireddyGiri/Brainrestall/main/repo/pool/main/b/brainrest"
-
   version "1.0.0"
-  sha256 "<SHA256_CHECKSUM_OF_FILE>"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   def install
     bin.install "brainrest"
