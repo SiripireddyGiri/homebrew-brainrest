f41c6c90cdb9f143b86543542c6e272a36c9040c
SiripireddyGiri <Siripireddygirikumar3@gmail.com>
2025-12-07 22:13:31 +0530

    Added path in brainrest.rb File

--- Formula/brainrest.rb
+++ Formula/brainrest.rb
@@ -1,7 +1,7 @@
 class Brainrest < Formula
   desc "Micro-break reminder tool for terminal users"
   homepage "https://github.com/SiripireddyGiri/Brainrestall"
-  url "https://raw.githubusercontent.com/SiripireddyGiri/Brainrestall/main/repo/pool/main/b/brainrest"
+  url "https://raw.githubusercontent.com/SiripireddyGiri/Brainrestall/main/brainrest"
   version "1.0.0"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
@@ -10,3 +10,4 @@ class Brainrest < Formula
     man1.install "brainrest.1"
   end
 end
+
